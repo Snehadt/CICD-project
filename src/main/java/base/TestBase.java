70b56fc67460be01c5dc61064b92b1be9568a98b
Snehadt <snehadt7@gmail.com>
2023-12-30 19:02:14 +0530

    Demo Test

--- src/main/java/base/TestBase.java
+++ src/main/java/base/TestBase.java
@@ -52,4 +52,8 @@ public class TestBase {
 
         driver.get(prop.getProperty("url"));
     }
+
+    public void demoTest(){
+        System.out.println("Hi.....");
+    }
 }
---
+++ src/test/java/testCase/DemoTest.java
@@ -0,0 +1,11 @@
+package testCase;
+
+import base.TestBase;
+import org.testng.annotations.Test;
+
+public class DemoTest extends TestBase {
+ @Test
+    public void initial(){
+     demoTest();
+ }
+}
